c06039ce3a35818a3cc0b25fffedbc291e85dec5
Bruno Sousa <bruno.luan.sousa@gmail.com>
2017-04-18 20:06:16 -0300

    Loading csv data into the table.

--- src/assets/js/basics.js
+++ src/assets/js/basics.js
@@ -3,25 +3,35 @@ var dados;
 
 // constrói o header da tabela
 function buildTableHeader (columnNames) {
-  table = d3.select("body").append("table").attr("border", 1);
-  console.log(table);
-  table.append("thead").append("tr").selectAll("th")
-    .data(columnNames).enter().append("th").text(function (d) {
-      return d;
+  table = d3.select("body").select("table");
+  table.append("thead").append("tr").selectAll("th").data(columnNames).enter().append("th").text(function (d) {
+    return d;
   });
 }
 
 // constrói o corpo da tabela
-function buildTableBody (data) {
-  //
+function buildTableBody (data, columns) {
+  var table = d3.select('body').select('table');
+  var tbody = table.append('tbody');
+
+  var rows = tbody.selectAll("tr").data(data).enter().append("tr");
+
+  var cells = rows.selectAll("td").data(function(row){
+    return columns.map(function(column) {
+      return {column: column, value: row[column]}
+    })
+  }).enter().append("td").text(function(d) {
+  	return d.value;
+  });
+
 }
 
-d3.csv('../../data/data.csv', function (data) {
-  dados = data;
-//  console.log(data);
-//  console.log(dados);
-});
-
-var columnNames = ['city','state','population','land area']
+var columnNames = ['city','state','population','land']
 
 buildTableHeader (columnNames);
+
+//carrega os dados do csv
+d3.csv('data/data.csv',function (data) {
+  buildTableBody(data, columnNames);
+})
+
