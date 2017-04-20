8f7757d5d08dc6a270c28b712f1ed7f51e15a646
Bruno Sousa <bruno.luan.sousa@gmail.com>
2017-04-20 17:23:03 -0300

    feature: sorting table data by column

--- src/assets/js/basics.js
+++ src/assets/js/basics.js
@@ -1,23 +1,22 @@
-// variáveis globais
+// global variables
 var data;
+var dataBackup;
 var columnNames;
+var columnOrder;
+var typeOrder = "ascending";
 
-// constrói o header da tabela
+//build the table header
 function buildTableHeader(columns) {
   columnNames = columns;
   var thead = d3.select("body").select("table").select("thead");
-  /*thead.append("tr").selectAll("th").data(columns).enter().append("th").text(function(d){
-    return d;
-  });*/
   thead.append("tr").selectAll("th").data(columns).enter().append("th").text(function(d){
     return d;
   }).append("i").attr("class", "pull-right glyphicon glyphicon-sort").attr("data-model", function(d){
       return d;
   });
-
 }
 
-// constrói o corpo da tabela
+// build the table body
 function buildTableBody(inputData, columns) {
   var tbody = d3.select('body').select('table').select('tbody');
 
@@ -25,26 +24,60 @@ function buildTableBody(inputData, columns) {
 
   var cells = rows.selectAll("td").data(function(row){
     return columns.map(function(column) {
+      row[column] = (row[column] != '') ? row[column] : "-";
       return {column: column, value: row[column]}
     })
   }).enter().append("td").text(function(d) {
   	return d.value;
   });
-
 }
 
-// carrega os dados do csv
-d3.csv('data/data.csv',function (inputData) {  
-  data = inputData;
+// load the csv data
+d3.csv('data/dados-tp1.csv',function (inputData) {  
+  dataBackup = inputData;
+  data = dataBackup;
   buildTableHeader(d3.keys(data[0]));
   buildTableBody(data, columnNames);  
   d3.select("#searchTextId").on("input", function() {search(this.value);});
   d3.selectAll(".glyphicon-sort").on("click", function() {sortTable($(this)); });
 })
 
-function sortTable(pattern){
-    console.log(pattern.attr("data-model"));
-        
+// sorting function that defines whether the data will be sorted in ascending or descending order
+function sortTable(headers){
+  if(columnOrder == headers.attr("data-model")){
+    if(typeOrder == 'ascending'){
+      descendingTable(headers);
+    }else{
+      ascendingTable(headers);
+    }
+  }else{
+    columnOrder = headers.attr("data-model");
+    ascendingTable(headers);
+  }
+  cleanTable();
+  buildTableBody(data, columnNames);
+}
+
+// sort the table data in ascending order
+function ascendingTable(headers){
+  data.sort(function(x, y) {
+    var column = headers.attr("data-model");
+    return (!isNaN(Number(x[column])) && !isNaN(Number(y[column]))) ? x[column]-y[column] : d3.ascending(x[column], y[column]);
+  });
+  typeOrder = "ascending";
+  $('.glyphicon').removeClass().addClass('pull-right glyphicon glyphicon-sort');
+  headers.removeClass('glyphicon-sort').addClass('glyphicon-sort-by-attributes');
+}
+
+// sort the table data in descending order
+function descendingTable(headers){
+  data.sort(function(x, y) {
+    var column = headers.attr("data-model");
+    return (!isNaN(Number(x[column])) && !isNaN(Number(y[column]))) ? y[column]-x[column] : d3.descending(x[column], y[column]);
+  });
+  typeOrder = "descending";
+  $('.glyphicon').removeClass().addClass('pull-right glyphicon glyphicon-sort');
+  headers.removeClass('glyphicon-sort').addClass('glyphicon-sort-by-attributes-alt');
 }
 
 // remove all rows from the table
@@ -55,7 +88,7 @@ function cleanTable() {
   }
 }
 
-// search for patter passed as parameter
+// search for pattern passed as parameter
 function search(pattern) {
   cleanTable();
   if (!pattern) {
